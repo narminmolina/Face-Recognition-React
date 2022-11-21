33c3cc0ac1b76f474397d2acd8ca14cf2912bae0
Narmin Molina <narminmolina@gmail.com>
2022-11-21 10:57:40 -0500

    form

--- src/App.js
+++ src/App.js
@@ -6,28 +6,15 @@ import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
-
+import Signin from "./components/Signin/Signin";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
-
-//You must add your own API key here from Clarifai.
+import Register from "./components/Register/Register";
 const app = new Clarifai.App({
   apiKey: "8b4354349e7a4c1c81a7509bec2bc63f",
 });
 
 console.log(app);
-// No Longer need this. Updated to particles-bg
-// const particlesOptions = {
-//   particles: {
-//     number: {
-//       value: 30,
-//       density: {
-//         enable: true,
-//         value_area: 800
-//       }
-//     }
-//   }
-// }
 
 class App extends Component {
   constructor() {
@@ -36,6 +23,8 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false,
     };
   }
 
@@ -72,55 +61,42 @@ class App extends Component {
       )
       .catch((err) => console.log(err));
   };
-  // onButtonSubmit = () => {
-  //   this.setState({ imageUrl: this.state.input });
-  //   app.models
-  //     .predict(
-  //       // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-  //       // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-  //       // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-  //       // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
-  //       // is to use a different version of their model that works like the ones found here: https://github.com/Clarifai/clarifai-javascript/blob/master/src/index.js
-  //       // so you would change from:
-  //       // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-  //       // to:
-  //       // .predict('53e1df302c079b3db8a0a36033ed2d15', this.state.input)
-  //       Clarifai.FACE_DETECT_MODEL,
-  //       this.state.input
-  //     )
-  //     .then((response) => {
-  //       console.log("hi", response);
-  //       if (response) {
-  //         fetch("http://localhost:3000/image", {
-  //           method: "put",
-  //           headers: { "Content-Type": "application/json" },
-  //           body: JSON.stringify({
-  //             id: this.state.user.id,
-  //           }),
-  //         })
-  //           .then((response) => response.json())
-  //           .then((count) => {
-  //             this.setState(Object.assign(this.state.user, { entries: count }));
-  //           });
-  //       }
-  //       this.displayFaceBox(this.calculateFaceLocation(response));
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
 
   render() {
     return (
       <div className="App">
         <Particle />
-
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              imageUrl={this.state.imageUrl}
+              box={this.state.box}
+            />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register />
+        )}
       </div>
     );
   }
